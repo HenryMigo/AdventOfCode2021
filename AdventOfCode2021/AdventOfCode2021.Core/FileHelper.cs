1e4fb57ac3332ed3652dbb5923927ba71f565982
Henry Migo <henryeamigo@gmail.com>
2021-12-01 22:44:54 +0000

    Initial File Setup

---
+++ AdventOfCode2021/AdventOfCode2021.Core/FileHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2021.Core;
+
+public static class FileHelper
+{
+    public static IEnumerable<int> ReadFile(string fileLocation)
+    {
+        var text = File.ReadAllLines(fileLocation);
+
+        return text.Select(int.Parse).ToList();
+    }
+}
