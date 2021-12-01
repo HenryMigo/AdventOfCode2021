46c65f09bc24f091c0b2e813d9b351773a7ca79f
Henry Migo <henryeamigo@gmail.com>
2021-12-01 23:02:56 +0000

    Day 1 Part One done

---
+++ AdventOfCode2021/DayOne.cs
@@ -0,0 +1,9 @@
+namespace AdventOfCode2021;
+
+public class DayOne
+{
+    public List<int> Process(List<int> input)
+    {
+        return new List<int>();
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AdventOfCode2021;
+using AdventOfCode2021.Core;
 
-Console.WriteLine("Hello, World!");
+var filePath = "/media/henrymigo/New Volume/dev/AdventOfCode2022/Files/";
+
+var dayOne = new DayOne();
+var dayOneData = FileHelper.ReadFile($"{filePath}DayOne.txt");
+
+var oneData = dayOneData as int[] ?? dayOneData.ToArray();
+
+int? previousValue = null;
+int increaseCount = 0;
+int decreaseCount = 0;
+
+for (var i = 0; i < oneData.Count(); i++)
+{
+    if (previousValue == null)
+    {
+        Console.WriteLine($"{oneData[i]} (N/A - no previous measurement)");
+    }
+    else
+    {
+        if (previousValue < oneData[i])
+        {
+            increaseCount++;
+        }
+        else if (previousValue > oneData[i])
+        {
+            decreaseCount++;
+        }
+    }
+
+    previousValue = oneData[i];
+}
+
+Console.WriteLine($"Increased: {increaseCount}");
+Console.WriteLine($"Decreased: {decreaseCount}");
+
+Console.ReadLine();
