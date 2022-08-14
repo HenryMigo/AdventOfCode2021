28dd3271aaab5922957e9e4348a9bc2b55b4b38f
Henry Migo <henryeamigo@gmail.com>
2022-08-14 14:25:26 +0100

    Day 6 Complete

--- AdventOfCode2021/Days/DaySix.cs
+++ AdventOfCode2021/Days/DaySix.cs
@@ -1,77 +1,35 @@
-﻿using MoreLinq;
-
-namespace AdventOfCode2021.Days;
+﻿namespace AdventOfCode2021.Days;
 
 public class DaySix
 {
-    private class LanternFish
+    const int MAX_LIFE_SIZE = 9; //size of the collection containing all days of the lifecycle. new fish are added at top of this cycle.
+    const int RESET_LIFE_IDX = 6; //how many days a re-spawned fish has left (7 days since it is 0 indexed)
+
+    public long GetTotalAmount(int days)
     {
-        private int InternalTimer { get; set; }
-        
-        public LanternFish(int internalTimer)
-        {
-            InternalTimer = internalTimer;
-        }
+        var fish = File.ReadAllLines(@"/dev/AdventOfCode2021/AdventOfCode2021/Files/DaySix.txt")
+            .SelectMany(input => input.Split(',').Select(int.Parse))
+            .GroupBy(f => f)
+            .Aggregate(new List<long>(new long[MAX_LIFE_SIZE]), (list, kv) =>
+            {
+                list[kv.Key] = kv.Count();
+                return list;
+            });
 
-        public void DecreaseInternalTimer(int amount)
-        {
-            InternalTimer -= amount;
-        }
-
-        public void SetInternalTimer(int amount)
-        {
-            InternalTimer = amount;
-        }
-
-        public int GetInternalTimer()
-        {
-            return InternalTimer;
-        }
-
-        public int Day { get; set; }
-        public int Total { get; set; }
-    }
-
-    private List<LanternFish> _lanternFishes;
-
-    public int GetTotalAmount(int days)
-    {
-        _lanternFishes = new List<LanternFish>();
-        
-        var fileInput = File.ReadAllLines(@"/dev/AdventOfCode2022/AdventOfCode2021/Files/DaySix.txt");
-        var inputSplit = fileInput[0].Split(',');
-
-        foreach (var input in inputSplit)
-        {
-            _lanternFishes.Add(new LanternFish(int.Parse(input)));
-        }
-
-        var tempStore = new List<LanternFish>(_lanternFishes);
-        
         for (var i = 0; i < days; i++)
         {
-            foreach (var lanternFishes in tempStore.Batch(1000))
-            {
-                var zeroTimers = lanternFishes.Where(x => x.GetInternalTimer() == 0);
-                var nonZeroTimers = lanternFishes.Where(x => x.GetInternalTimer() != 0);
-                
-                foreach (var zeroTimer in zeroTimers)
-                {
-                    zeroTimer.SetInternalTimer(6);
-                    _lanternFishes.Add(new LanternFish(8));
-                }
+            var expired = fish.First();
 
-                foreach (var nonZeroTimer in nonZeroTimers)
-                {
-                    nonZeroTimer.DecreaseInternalTimer(1);
-                }
-            }
+            //shift list to decrement each fishes life
+            fish.RemoveAt(0);
 
-            tempStore = new List<LanternFish>(_lanternFishes);
-            
-            Console.WriteLine($"Day {i}");
+            //reset the lifecycle of the expired fish
+            fish[RESET_LIFE_IDX] += expired;
+
+            //each expired fish creates a new fish at the top of the life cycle
+            fish.Add(expired);
         }
 
-        return _lanternFishes.Count;
+        return fish.Sum(c => c);
     }
 }
